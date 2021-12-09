ae0908fa0c1815865c6c2bf0c1c669b21d69f9c5
mandie021-moEqdO <mandie021@outlook.com>
2021-12-09 08:46:34 -0500

    adds

--- scrape_mars.py
+++ scrape_mars.py
@@ -10,28 +10,78 @@ import requests
 conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
 
-def scrape():
-    # browser = init_browser()
+def main_scrape():
+    # Setup splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser3 = Browser('chrome', **executable_path, headless=False)
-    mars_url = 'https://marshemispheres.com/'
-    browser3.visit(mars_url)
-    html = browser3.html
-    soup = bs(html, "html.parser")
-
-    hemisphere_image_urls = {}
-    hemisphere_image_urls['title'] = []
-    hemisphere_image_urls['img_url'] = []
-    for item in product_search:
-        img_search=soup.find('img', class_='thumb')
-        hemisphere_image_urls['img_url'].append('https://marshemispheres/' + img_search['src'])
-        title_search=soup.find('h3').text
-        hemisphere_image_urls['title'].append(title_search)
-        print(hemisphere_image_urls)
+    browser = Browser('chrome', **executable_path, headless=False)
 
 
-    
     # Quit the browser
     browser.quit()
 
+def mars_news(browser):
+    ##url 
+    url = 'https://redplanetscience.com/'
+    browser.visit(url)
+
+    #beautiful soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    cl_row = soup.select_one('div.row')
+    #grab news title
+    news_title= cl_row.select_one('div.content_title').text
+    #grab news paragrath
+    news_p = cl_row.select_one('div.article_teaser_body').text
+
+
+def space_feat(browser):
+    #url
+    url = 'https://spaceimages-mars.com/'
+    browser.visit(url)
+
+    #beautiful soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    results = soup.find('img', class_="headerimage")
+    img_src = results.attrs['src']
+    featured_image_url = url+img_src
+
+
+def mars_facts()
+    #url
+    url = 'https://galaxyfacts-mars.com/'
+    mars_tables = pd.read_html(str(url), header=None, index_col=None)
+    mars_info = mars_tables[1]
+    mars_info.columns = ["Mars Profile", "Measure"]
+    mars_web = mars_info.to_html()
+    mars_earth_info = mars_tables[0]
+    mars_earth_info.columns = ["Mars - Earth Comparison", "Mars", "Earth"]
+    mars_earth_web = mars_earth_info.to_html()
+
+
+
+def img_scrape():
+    # url % beautiful soup 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    hemisphere_image_urls = []
+
+    for item in range(4):
+        browser.find_by_css('a.product-item h3')[item].click()
+        sample = browser.find_link_by_text('Sample').first
+        img_url = sample['href']
+        title_search=browser.find_by_css("h2.title").text
+        hemisphere_image_urls.append({"title": title_search,'img_url':'https://marshemispheres/' + url})
+        print(hemisphere_image_urls)
+        browser.back()
+
     return hemisphere_image_urls
+    
+    
+
+    
