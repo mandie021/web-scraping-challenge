2019df249c8423532ba261f6ec22ee8d978f434e
mandie021-moEqdO <mandie021@outlook.com>
2021-12-09 16:09:01 -0500

    12/9

--- scrape_mars.py
+++ scrape_mars.py
@@ -2,8 +2,8 @@ from splinter import Browser
 from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
-import ssl
-import requests
+#import ssl
+#import requests
 
 
 # setup mongo connection
@@ -14,10 +14,28 @@ def main_scrape():
     # Setup splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
+    
+    news_title, news_p = mars_news(browser)
+    feature_img = space_feat(browser)
+    mars_web, mars_earth_web = mars_facts()
+    hemisphere_image_urls = img_scrape(browser)
 
+
+    mars_data= {
+                "Mars News" : news_title, 
+                "Mars Paragraph" : news_p, 
+                'Featured Image' : feature_img,
+                "Mars Table" : mars_web,
+                "Mars Earth Web" : mars_earth_web,
+                "Hemisphere Images Titles" : hemisphere_image_urls
+                }
 
     # Quit the browser
     browser.quit()
+
+    return mars_data
+
+    
 
 def mars_news(browser):
     ##url 
@@ -34,6 +52,7 @@ def mars_news(browser):
     #grab news paragrath
     news_p = cl_row.select_one('div.article_teaser_body').text
 
+    return news_title, news_p
 
 def space_feat(browser):
     #url
@@ -48,8 +67,10 @@ def space_feat(browser):
     img_src = results.attrs['src']
     featured_image_url = url+img_src
 
+    return featured_image_url
 
-def mars_facts()
+
+def mars_facts(): 
     #url
     url = 'https://galaxyfacts-mars.com/'
     mars_tables = pd.read_html(str(url), header=None, index_col=None)
@@ -60,9 +81,10 @@ def mars_facts()
     mars_earth_info.columns = ["Mars - Earth Comparison", "Mars", "Earth"]
     mars_earth_web = mars_earth_info.to_html()
 
+    return mars_web, mars_earth_web
 
 
-def img_scrape():
+def img_scrape(browser):
     # url % beautiful soup 
     url = 'https://marshemispheres.com/'
     browser.visit(url)
@@ -76,7 +98,7 @@ def img_scrape():
         sample = browser.find_link_by_text('Sample').first
         img_url = sample['href']
         title_search=browser.find_by_css("h2.title").text
-        hemisphere_image_urls.append({"title": title_search,'img_url':'https://marshemispheres/' + url})
+        hemisphere_image_urls.append({"title": title_search,'img_url':'https://marshemispheres/' + img_url})
         print(hemisphere_image_urls)
         browser.back()
 
